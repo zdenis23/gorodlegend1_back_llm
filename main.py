fd1fd35c8a9fcdb8ec2cc52bd8a3fe8aac881521
Denis Zemskikh <zemskikhdenis@gmail.com>
2024-03-17 11:29:58 +0300

    llm

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException, status
+
+from app.chatbot import Chatbot
+from app.mistral_retrieval_qa import retrieve_from_str
+import uvicorn
+
+app = FastAPI()
+
+chatbot = Chatbot()
+
+
+@app.post("/talk")
+def talk(input: str):
+    response = chatbot.generate_response(input)
+    if response is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No response found"
+        )
+    return {"response": response}
+
+
+@app.post("/retrieve")
+def retrieve(query: str):
+    answer = retrieve_from_str(query)
+    if answer is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No answer found"
+        )
+    return {"answer": answer}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
